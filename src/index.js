90615c61761706a1a77f137048f67435084940b0
sidhanathdon <137877586+sidhanathdon@users.noreply.github.com>
2023-11-02 19:27:52 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 //Write your code here
+class API{
+    #secure=true;
+    constructor(url, method='GET')
+    {
+        this.url=url
+        this.method=method
+        this.#secure=url.startsWith('https')
+    }
+    isSecure(){
+        return this.#secure
+    }
+    updateUrl(newUrl) {
+        this.url = newUrl;
+        this.#secure = newUrl.startsWith('https');
+    }
 
+}
 
+// const s = new API('http://api.com/api/hello');
+// console.log(s.isSecure()); // false
+
+// s.updateUrl('https://api.com/api/hello');
+// console.log(s.isSecure()); // true
+
+// console.log(s.url); // https://api.com/api/hello
+// console.log(s.method); // GET
+// console.log(s.secure); // undefined (because it's a private field)
 
 
 
